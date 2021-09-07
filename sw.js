9de1a9c95c2929753d59ceeaf1aeb2caf8cc53a5
KariukiAbel <abelmuriithi@gmail.com>
2021-09-07 19:43:44 +0300

    Doing pre caching for frequently used items but there arises an issue that the service worker might be installed in a splint second and we may not be sure that the assets are cached but I have a work around it

--- sw.js
+++ sw.js
@@ -1,14 +1,31 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    'css/materialize.min.css',
+    '/img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+];
+
 //install the service worker
 self.addEventListener('install', evt => {
-    console.log("service worker has been installed")
+    //console.log("service worker has been installed")
+    caches.open(staticCacheName).then(cache => {
+        console.log('caching shell assets')
+        cache.addAll(assets)
+    })
 });
 
 // activate service worker
 self.addEventListener('activate', evt => {
-    console.log('service worker has been activated')
+    //console.log('service worker has been activated')
 });
 
 // fetch events
 self.addEventListener('fetch', evt => {
-    console.log('fetch event ', evt)
+    //console.log('fetch event ', evt)
 })
