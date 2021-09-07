3114d5ebe759960e20279df9cfbc85adc25aefca
KariukiAbel <abelmuriithi@gmail.com>
2021-09-07 17:17:01 +0300

    Embacked on this project though I see the materialize isn't working nicely

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     //nav menu
     const menus = document.querySelectorAll('.side-menu');
-    M.Sidenav.init(menus, { edges: 'right' });
+    var init = M.Sidenav.init(menus, { edge: 'right' });
     //add recipe form
     const forms = document.querySelectorAll('.side-form');
-    M.Sidenav.init(forms, { edge: 'bottom' });
+    M.Sidenav.init(forms, { edge: 'left' });
 });
