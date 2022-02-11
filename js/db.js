4a4e750de0c37cc9f1ea61e628f74d463f85b650
KariukiAbel <abelmuriithi@gmail.com>
2022-02-11 12:46:49 +0300

    Made sure that data from firebase is displayed on the ui

--- js/db.js
+++ js/db.js
@@ -4,7 +4,7 @@ console.log(snapshot.docChanges());
 snapshot.docChanges().forEach(change => {
 console.log(change, change.doc.data(), change.doc.id);
 if (change.type === "added") {
-
+  renderRecipe(change.doc.data(), change.doc.id)
 }
 if (change.type === 'removed') {
 
